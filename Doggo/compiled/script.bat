9e0307e9bf93acaae9ef55da6b7f433aadca85b7
Arthur Pêtre <arthur30700@gmail.com>
2021-12-13 18:30:24 +0100

    finito javacc

---
+++ Doggo/compiled/script.bat
@@ -0,0 +1,1 @@
+javacc -OUTPUT_DIRECTORY:F:/GITS/GitHub/Doggo-Programming-Language/Doggo/compiled/ ../Doggo.jj && echo ---------------------------------------------------------- && javac ./Doggo.java
